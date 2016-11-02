5fd78a17f5bd9178d1dc878e0b2db0dbce655f26
Jonathan Lurie <lurie.jo@gmail.com>
2016-11-02 17:26:34 -0400

    starting a scene

---
+++ src/Scene.js
@@ -0,0 +1,13 @@
+// take some inspiration here:
+// https://threejs.org/examples/webgl_multiple_views.html
+
+class Scene{
+
+  constructor(){
+    var container;
+  var camera, scene, renderer;
+  }
+
+}
+
+export { scene };
